64a0eacb1e04109e05b4e203caf61eefd25a2e81
Bradley Carestia <bradley.dotnet@gmail.com>
2024-06-14 18:13:40 -0700

    standalone bootstrap

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { provideHttpClient } from "@angular/common/http";
+import { ApplicationConfig } from "@angular/core";
+
+export const appConfig: ApplicationConfig = {
+    providers: [
+        provideHttpClient()
+    ]
+}
